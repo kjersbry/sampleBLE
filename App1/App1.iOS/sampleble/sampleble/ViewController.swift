32f7765df7f22daa42636d6e9d15f5d1430eec6b
kjersti.brynestad <kjersti.brynestad@imatis.com>
2019-08-06 13:55:47 +0200

    ..

---
+++ App1/App1.iOS/sampleble/sampleble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sampleble
+//
+//  Created by Imatis on 06/08/2019.
+//  Copyright © 2019 Imatis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- App1/App1/MainPage.xaml.cs
+++ App1/App1/MainPage.xaml.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using Xamarin.Forms;
+using System.Diagnostics;
 
 namespace App1
 {
@@ -14,10 +12,15 @@ namespace App1
         {
             InitializeComponent();
             ble = new Bluetooth();
+            Plugin.BLE.Abstractions.Trace.TraceImplementation = (format, @params) =>
+            {
+                Debug.WriteLine($"{DateTime.Now}: {format}", @params);
+            };
         }
 
         async void OnButtonClickSearch(object sender, System.EventArgs e)
         {
+            Console.WriteLine("start sleeping");
             Thread.Sleep(5000);
             Console.WriteLine("done sleeping");
             await ble.ScanForDevice("46a970e0-0d5f-11e2-8b5e-0002a5d5c51b");
