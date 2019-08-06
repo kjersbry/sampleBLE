8203ae89fa80b6e1976dcd5ae551781b45c5c062
Kjersti Brynestad <kjersti.brynestad@imatis.com>
2019-08-06 12:53:48 +0200

    first

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        Bluetooth ble;
+        public MainPage()
+        {
+            InitializeComponent();
+            ble = new Bluetooth();
+        }
+
+        async void OnButtonClickSearch(object sender, System.EventArgs e)
+        {
+            Thread.Sleep(5000);
+            Console.WriteLine("done sleeping");
+            await ble.ScanForDevice("46a970e0-0d5f-11e2-8b5e-0002a5d5c51b");
+        }
+    }
+}
---
+++ App1/App1/minimalBluetooth.cs
@@ -0,0 +1,54 @@
+﻿using Plugin.BLE;
+using Plugin.BLE.Abstractions;
+using Plugin.BLE.Abstractions.Contracts;
+using Plugin.BLE.Abstractions.Exceptions;
+using System;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+//using NuGet Plugin.BLE
+
+namespace App1
+{
+    public class Bluetooth
+    {
+        IBluetoothLE ble;
+        public BluetoothState State { get { return ble.State; } }
+        IAdapter adapter;
+        ObservableCollection<IDevice> deviceList;
+
+        public Bluetooth()
+        {
+            ble = CrossBluetoothLE.Current;
+            adapter = CrossBluetoothLE.Current.Adapter;
+            deviceList = new ObservableCollection<IDevice>();
+            adapter.ScanTimeout = 60000;
+        }
+
+        public async Task<IDevice> ScanForDevice(string wantedServiceUUID)
+        {
+            deviceList.Clear();
+            IDevice device = null;
+
+            //Device discovery event handler
+            adapter.DeviceDiscovered += (s, a) =>
+            {
+                deviceList.Add(a.Device);
+                Console.WriteLine("Discovered periperhal. Name: \"" + a.Device.Name + " \"");
+
+                adapter.StopScanningForDevicesAsync();
+                device = a.Device;
+            };
+
+            if (!ble.Adapter.IsScanning)
+            {
+                Console.WriteLine("Starting scan for " + wantedServiceUUID);
+                await adapter.StartScanningForDevicesAsync(new Guid[] { Guid.Parse(wantedServiceUUID) });
+                Console.WriteLine("Scan for " + wantedServiceUUID + " ended");
+            }
+            return device;
+        }
+    }
+}
