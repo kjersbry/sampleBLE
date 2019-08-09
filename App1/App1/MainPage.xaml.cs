2e825e07112d8d73710e3eed19df1b9cbc68a340
kjersti.brynestad <kjersti.brynestad@imatis.com>
2019-08-09 14:37:44 +0200

    .

--- App1/App1/MainPage.xaml.cs
+++ App1/App1/MainPage.xaml.cs
@@ -7,11 +7,11 @@ namespace App1
 {
     public partial class MainPage : ContentPage
     {
-        static public Bluetooth ble;
+        static public Bluetooth Bluetooth;
         public MainPage()
         {
             InitializeComponent();
-            ble = new Bluetooth();
+            Bluetooth = new Bluetooth();
             Plugin.BLE.Abstractions.Trace.TraceImplementation = (format, @params) =>
             {
                 Debug.WriteLine($"{DateTime.Now}: {format}", @params);
@@ -23,7 +23,7 @@ namespace App1
             Console.WriteLine("start sleeping");
             Thread.Sleep(5000);
             Console.WriteLine("done sleeping");
-            await ble.ScanForDevice("46a970e0-0d5f-11e2-8b5e-0002a5d5c51b");
+            await Bluetooth.ScanForDevice("46a970e0-0d5f-11e2-8b5e-0002a5d5c51b");
         }
     }
 }
